61c31ad131ccad5dbaa842514156219e6bf42a90
Henry <146377665+henryw-ithers@users.noreply.github.com>
2026-03-29 22:01:35 -0400

    feat(frontend): redo navbar and add non-functional search bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,75 @@ import { Link, Route, Routes } from "react-router-dom";
 import { DashboardPage } from "./pages/DashboardPage";
 import { AddTransactionPage } from "./pages/AddTransactionPage";
 import { CategoriesPage } from "./pages/CategoriesPage";
+import DashboardChart from "./components/DashboardChart";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="app">
-      <nav className="nav">
-        <Link to="/">Dashboard</Link>
-        <Link to="/transactions/new">Add Transaction</Link>
-        <Link to="/categories">Categories</Link>
-      </nav>
+      <div>
+        <header className="navbar navbar-dark sticky-top bg-dark shadow px-3">
+          <div className="container-fluid d-flex align-items-center justify-content-between">
+            <a className="navbar-brand mb-0" href="#">
+              Finance Tracker
+            </a>
 
-      <main className="container">
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/transactions/new" element={<AddTransactionPage />} />
-          <Route path="/categories" element={<CategoriesPage />} />
-        </Routes>
-      </main>
-    </div>
+            <input
+              className="form-control w-50 mx-3"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+            />
+
+            <button
+              className="navbar-toggler d-md-none"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#sidebarMenu"
+              aria-controls="sidebarMenu"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
+        </header>
+
+        <div className="container-fluid">
+          <div className="row">
+            <nav
+              id="sidebarMenu"
+              className="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+            >
+              <div className="position-sticky pt-3">
+                <ul className="nav flex-column">
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">
+                      Dashboard
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/add">
+                      Add Transaction
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/categories">
+                      Categories
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+
+            <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4 py-4">
+              <Routes>
+                <Route path="/" element={<DashboardPage />} />
+                <Route path="/add" element={<AddTransactionPage />} />
+                <Route path="/categories" element={<CategoriesPage />} />
+              </Routes>
+            </main>
+          </div>
+        </div>
+      </div>
   );
 }
